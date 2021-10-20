bd72ff625184853834b3bd5a2ba0f49a686dd35c
UnlightedOtaku <45728883+UnlightedOtaku@users.noreply.github.com>
2021-10-20 16:47:45 +0800

    Update downloadsmallgif.py
    改变了UA，并且增加了一条说明。

--- downloadsmallgif.py
+++ downloadsmallgif.py
@@ -8,13 +8,14 @@ import time
 
 
 # 首先普通请求一次，找到imgHostKey和totalPage 分辨率大小size
-p_code = '7844760731971'
+p_code = '6913506539924'
 url = f'https://m.doc88.com/p-{p_code}.html'
 header = {
     'Referer': 'http://m.doc88.com/',
-    'User-Agent': 'Mozilla/5.0 (Linux; Android 9; ONEPLUS A6010 Build/PKQ1.180716.001; wv) AppleWebKit/537.36 (KHTML, '
-                  'like Gecko) Version/4.0 Chrome/76.0.3809.89 Mobile Safari/537.36 T7/11.19 SP-engine/2.15.0 '
-                  'baiduboxapp/11.19.0.11 (Baidu; P1 9) '
+    # 'User-Agent': 'Mozilla/5.0 (Linux; Android 9; ONEPLUS A6010 Build/PKQ1.180716.001; wv) AppleWebKit/537.36 (KHTML, '
+    #               'like Gecko) Version/4.0 Chrome/76.0.3809.89 Mobile Safari/537.36 T7/11.19 SP-engine/2.15.0 '
+    #               'baiduboxapp/11.19.0.11 (Baidu; P1 9) '
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.114 Mobile Safari/537.36'
 }
 result = requests.get(url,headers=header)
 return_data = result.text
@@ -22,6 +23,7 @@ return_data = result.text
 
 # 一张图片的url为： https://{imgHostKey}.doc88.com/p.do?id={pcode}-{page}-{resolution}-0-4-{part}(可以为00,01,10,11对应左上，右上，左下，右下)-2-1-{hour}(当前小时数)
 # https://mt7.doc88.com/p.do?id=7844760731971-1-960-0-4-00-2-1-15
+
 
 # 使用正则表达式从返回结果中找到imgHostKey和totalPage
 pattern1 = 'var imgHostKey = ".*"'
@@ -34,12 +36,17 @@ totalPage = int(result2.group(0).split('=')[-1].strip().strip('"'))
 print(totalPage)
 resolution_height = r'var resolution_height = \d*'
 resolution_height = re.search(resolution_height,return_data)
+
 resolution_height = int(resolution_height.group(0).split('=')[-1].strip())
 resolution_width = r'var resolution_width = \d*'
 resolution_width = re.search(resolution_width,return_data)
+
 resolution_width = int(resolution_width.group(0).split('=')[-1].strip())
-size = min(resolution_height,resolution_width)
-print(size)
+
+# size = min(resolution_height,resolution_width)
+size = max(resolution_height,resolution_width)
+print("使用imgswitch时记得改size为:%s"%size)
+
 
 # 获取当前小时
 hour = datetime.datetime.now().strftime("%H")
